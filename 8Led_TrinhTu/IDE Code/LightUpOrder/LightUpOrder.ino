51ad9b2a98a6d15cfbbb30edff9efe99805cd304
Nguyen Quoc Chau <chauquocnguyen.cun1@gmail.com>
2021-10-08 21:00:29 +0700

    lightuporder

---
+++ 8Led_TrinhTu/IDE Code/LightUpOrder/LightUpOrder.ino
@@ -0,0 +1,18 @@
+#define _clock 7
+#define _latch 6
+#define _data 5
+void setup() {
+  pinMode( _clock,OUTPUT);
+  pinMode( _latch,OUTPUT);
+  pinMode( _data,OUTPUT);
+}
+
+void loop() {
+  for ( int i=0; i< 256; i++)
+  {
+    digitalWrite(_latch, LOW);
+    shiftOut(_data, _clock, LSBFIRST,i);
+    digitalWrite(_latch, HIGH);
+    delay(100);
+  }
+}
